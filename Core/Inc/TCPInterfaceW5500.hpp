0261859cfae7469e91d04f32be6d06b59122378c
broms_home <orizonti@inbox.com>
2024-02-15 14:14:54 +0300

    Start project

---
+++ Core/Inc/TCPInterfaceW5500.hpp
@@ -0,0 +1,13 @@
+#ifndef MY_CLASS
+#define MY_CLASS
+
+int TestFunction();
+
+template< typename T>
+class TestClassTT
+{
+public:
+	T x= 0;
+};
+
+#endif //MY_CLASS
---
+++ Core/Src/TCPInterfaceW5500.hpp
@@ -0,0 +1,7 @@
+#include <TCPInterfaceW5500.hpp>
+
+
+int TestFunction()
+{
+return 0;
+}
